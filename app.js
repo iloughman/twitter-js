df143435648020b515c702f5c76bfe2fe62215e1
iloughman <iloughman@gmail.com>
2015-01-23 17:32:50 -0500

    Individual Tweet Views

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var express = require( 'express' );
 var morgan = require( 'morgan' );
+var swig = require('swig');
+var routes = require('./routes/');
+var bodyParser = require('body-parser')
+
 
 var app = express();
 
 app.use(morgan('dev'));
+app.use('/', routes);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static('public'));
+
+app.engine('html',swig.renderFile);
+app.set('view engine', 'html');
+app.set('views',__dirname + '/views');
+swig.setDefaults({ cache: false });
 
 var server = app.listen(3000, function () {
 
@@ -14,6 +32,3 @@ var server = app.listen(3000, function () {
 
 })
 
-app.get('/', function (req, res) {
-  res.send('Hello World! How are you?')
-})
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var tweetBank = require('../tweetBank');
+var morgan = require( 'morgan' );
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
+});
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var tweets = tweetBank.find( {name: name} );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: tweets } );
+});
+
+module.exports = router;
